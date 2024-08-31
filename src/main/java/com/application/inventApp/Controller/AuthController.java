3d5aef0973e2d249369f2f59f8575e1ac6978846
Yocmar Rafael Jiménez González <yocmarjigon@gmail.com>
2024-08-30 20:50:08 -0500

    Prueba de conflictos entre ramas #4

--- src/main/java/com/application/inventApp/Controller/AuthController.java
+++ src/main/java/com/application/inventApp/Controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RestController
 @RequestMapping("/auth")
+/* dksdksdjkksd */
 public class AuthController {
   @Autowired
   private UserService userService;
